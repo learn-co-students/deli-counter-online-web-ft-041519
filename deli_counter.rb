4fef6d5f19f934fbf7afe219101491c48ed403b0
Annia Forney <anniaforney@gmail.com>
2019-04-20 19:31:43 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  return "The line is currently empty."
+end 
+
+def take_a_number(katz_deli, "Annia") 
+ return katz_deli  
+end 
+
+def take_a_number(katz_deli, "Derek") 
+ return katz_deli  
+end 
+
+def take_a_number(katz_deli, "Penny") 
+ return katz_deli  
+end 
+
+def take_a_number(katz_deli, "Michael") 
+ return katz_deli  
+end 
+
+def now_serving(katz_deli)
+end 
+
+def line(katz_deli)
+end 
